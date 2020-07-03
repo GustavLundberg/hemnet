a15035d0db53fbe88ae205f625d7a4be4edb0b57
GustavLundberg <karl.gustav.lundberg@gmail.com>
2020-07-03 12:42:27 +0200

    Scrapes links from search page and also scrapes correct div and p tags for detailed view

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,41 @@
-from selenium import webdriver
+import requests
+#from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 
+url = 'https://www.hemnet.se/bostader?location_ids%5B%5D=17989&item_types%5B%5D=bostadsratt'
+page = requests.get(url)
+results = BeautifulSoup(page.content, 'html.parser')
 
+#housing_elems = results.find_all('li', class_ = 'normal-results__hit js-normal-list-item')
+#print(type(housing_elems), len(housing_elems), '\n', housing_elems[0])
 
-print('Hello world')
-print('Hello world')
-a = pd.read_csv('')
+housing_elems = results.find_all('a', class_ = 'js-listing-card-link listing-card')
+
+links = []
+
+for housing_elem in housing_elems:
+	links.append(housing_elem['href'])
+
+for link in links:
+	print(type(link), link)
+
+################################
+
+# Scraping details page
+
+url = 'https://www.hemnet.se/bostad/lagenhet-2rum-rorsjostaden-malmo-kommun-foreningsgatan-50a-16907356'
+page = requests.get(url)
+results = BeautifulSoup(page.content, 'html.parser')
+
+address_div = results.find_all('div', class_ = 'property-address')
+price_div = results.find_all('p', class_ = 'property-info__price qa-property-price')
+info_div = results.find_all('div', class_ = 'property-info__attributes-and-description')
+
+print('-----------------------------------------------------------------')
+print(type(address_div), '\n', address_div)
+print('-----------------------------------------------------------------')
+print(type(price_div), '\n', price_div)
+print('-----------------------------------------------------------------')
+print(type(info_div), '\n', info_div)
+
