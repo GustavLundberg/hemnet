45fe986f589faa856940492e1398267f48ce5912
GustavLundberg <karl.gustav.lundberg@gmail.com>
2020-07-06 11:16:57 +0200

    Added function for getting details

--- webscraper.py
+++ webscraper.py
@@ -1,7 +1,10 @@
 import requests
 #from selenium import webdriver
+import unicodedata
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
+import random
 
 url = 'https://www.hemnet.se/bostader?location_ids%5B%5D=17989&item_types%5B%5D=bostadsratt'
 page = requests.get(url)
@@ -21,74 +24,95 @@ for housing_elem in housing_elems:
 #	print(type(link), link)
 
 ################################
-
 # Scraping details page
 
-url = 'https://www.hemnet.se/bostad/lagenhet-2rum-rorsjostaden-malmo-kommun-foreningsgatan-50a-16907356'
-page = requests.get(url)
-results = BeautifulSoup(page.content, 'html.parser')
+#url = 'https://www.hemnet.se/bostad/lagenhet-2rum-rorsjostaden-malmo-kommun-foreningsgatan-50a-16907356'
 
-address_div = results.find('div', class_ = 'property-address')
-price_p = results.find('p', class_ = 'property-info__price qa-property-price')
-info_div = results.find('div', class_ = 'property-info__attributes-and-description')
+def get_details(url):
 
+	details = {}
 
-# Extracting the address and area
-address = address_div.find('h1', class_ = 'qa-property-heading hcl-heading hcl-heading--size2 hcl-heading--reset-margin').text
-#print(type(address), address)
+	page = requests.get(url)
+	results = BeautifulSoup(page.content, 'html.parser')
 
-area = address_div.find('span', class_ = 'property-address__area').text
-#print(type(area), area)
+	address_div = results.find('div', class_ = 'property-address')
+	price_p = results.find('p', class_ = 'property-info__price qa-property-price')
+	info_div = results.find('div', class_ = 'property-info__attributes-and-description')
 
 
-# Extracing the price and converting to int
-price = price_p.text
-price = [x for x in price if x in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']]
-price = ''.join(price)
-price = int(price)
-#print(type(price), price)
+	# Extracting the address and area
+	address = address_div.find('h1', class_ = 'qa-property-heading hcl-heading hcl-heading--size2 hcl-heading--reset-margin').text
+	details['address'] = address
+
+	area = address_div.find('span', class_ = 'property-address__area').text
+	details['area'] = area
+
+
+	# Extracing the price and converting to int
+	price = price_p.text
+	price = [x for x in price if x in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']]
+	price = ''.join(price)
+	price = int(price)
+	details['price'] = price
 
 
 
 
-# Extracting the info attributes and description
-print('-----------------------------------------------------------------')
-print(type(info_div), '\n', info_div)
+	# Extracting the info attributes and description
+	property_type = info_div.find('dd', class_ = 'property-attributes-table__value').text
+	tenure_type = info_div.find('dd', class_ = 'property-attributes-table__value').text
 
-property_type = info_div.find('dd', class_ = 'property-attributes-table__value').text
-tenure_type = info_div.find('dd', class_ = 'property-attributes-table__value').text
+	details['property_type'] = property_type
+	details['tenure_type'] = tenure_type
 
-#print(type(tenure_type), tenure_type)
+	dt = info_div.find_all('dt', class_ = 'property-attributes-table__label')
+	dd = info_div.find_all('dd', class_ = 'property-attributes-table__value')
 
-print('-----------------------------------------------------------------')
+	dt = [x.text.strip() for x in dt]
+	dd = [x.text.strip() for x in dd]
 
-#for div in info_div.find_all('div'):
-#	c = div['class']
-#	if 'property-attributes-table__row' in div['class'][0]:
-#		print(type(div), div)
-	#print('c = ', type(c), c)
-#	print('-----------------------------------------------------------------')
-	#print(type(div), div)
-	#div.find('dd', class_ = 'property-attributes-table__value').text
+	dt = dt[:-1]
+	dd = dd[:-1]
 
-dt = info_div.find_all('dt', class_ = 'property-attributes-table__label')
-dd = info_div.find_all('dd', class_ = 'property-attributes-table__value')
+	attributes = {key: value for key, value in zip(dt, dd)}
 
-dt = [x.text.strip() for x in dt]
-dd = [x.text.strip() for x in dd]
+	details.update(attributes)
+	#print('AFTER - ', details)
 
-dt = dt[:-1]
-dd = dd[:-1]
+	popularity = info_div.find_all('div', class_ = 'property-visits-counter__row-value')
+	popularity = [x.text for x in popularity]
 
-attributes = {key: value for key, value in zip(dt, dd)}
-print(attributes) # Cleaning up later
+	print('POP = ', popularity)
+	
+	try:
+		visits = popularity[0]
+		days_available = popularity[1]	
+	except Exception as e:
+		visits = None
+		days_available = None
+	
+	details['visits'] = visits
+	details['days_available'] = days_available	
+	
 
-description = info_div.find('div', class_ = 'property-description js-property-description property-description--long').text.strip()
-print(type(description), description)
+	description = info_div.find('div', class_ = 'property-description js-property-description property-description--long').text.strip()
+	#print(type(description), description)
+	details['description'] = description
 
-popularity = info_div.find_all('div', class_ = 'property-visits-counter__row-value')
-popularity = [x.text for x in popularity]
-print(popularity)
+	#popularity = [unicodedata.normalize("NFKD", el) for el in popularity]
+	#print(popularity)
 
-new_str = unicodedata.normalize("NFKD", popularity[0])
-print(new_str)
+	#new_str = unicodedata.normalize("NFKD", popularity[0])
+	#new_str = int(new_str)
+	#print(type(new_str), new_str)
+
+	return details
+
+for link in links:
+	print('link = ', link)
+	#print(get_details('https://www.hemnet.se/bostad/lagenhet-2rum-mollevangen-malmo-kommun-simrishamnsgatan-7-b-16886300'))
+	print(get_details(link))
+	r = random.uniform(0, 5)
+	time.sleep(r)
+
+#details = get_details('https://www.hemnet.se/bostad/lagenhet-2rum-rorsjostaden-malmo-kommun-foreningsgatan-50a-16907356')
