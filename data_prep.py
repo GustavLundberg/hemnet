965d9a2886deb7cecc94f2def16b75cf6264a215
GustavLundberg <karl.gustav.lundberg@gmail.com>
2020-07-07 11:57:55 +0200

    Added some further transformations on the pandas df

--- data_prep.py
+++ data_prep.py
@@ -8,10 +8,13 @@ print(df.info())
 #print(df.dtypes())
 print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
 
-
-# Applying some transformation on the columns to get them in the desired format
+# Applying transformations on the columns to get them in the desired format
+df['area'] = df['area'].apply(lambda x: 'Ön' if re.match('(\s|^)(Ön|ön).*', x) else x)
+df['area'] = df['area'].apply(lambda x: re.sub('(,|/|\(|\-).*$', '', x).strip())
 df['Antal rum'] = df['Antal rum'].apply(lambda x: str.split(x)[0])
 df['Boarea'] = df['Boarea'].apply(lambda x: str.split(x)[0].replace(',', '.'))
+df['Byggår'] = df['Byggår'].apply(lambda x: x if isinstance(x, str) else 2020)										# Assuming that property without Byggår is newly built
+df['Byggår'] = df['Byggår'].apply(lambda x: x.split('-')[-1] if isinstance(x, str) else x)							# Using the latest year as Byggår
 df['Balkong'] = df['Balkong'] == 'Ja'
 df['Förening'] = df['Förening'].apply(lambda x: re.sub('\n\nOm föreningen', '', x) if isinstance(x, str) else None)
 df['Avgift'] = df['Avgift'].apply(lambda x: price_str_to_int(x) if isinstance(x, str) else -1)
@@ -19,14 +22,14 @@ df['Driftkostnad'] = df['Driftkostnad'].apply(lambda x: price_str_to_int(x) if i
 df['Kvadratmeterpris'] = df['Kvadratmeterpris'].apply(lambda x: price_str_to_int(x) if isinstance(x, str) else -1)
 df['visits'] = df['visits'].apply(lambda x: price_str_to_int(x) if isinstance(x, str) else -1)
 df['days_available'] = df['days_available'].apply(lambda x: price_str_to_int(x) if isinstance(x, str) else -1)
+df['avg_daily_visits'] = df.apply(lambda x: round(x['visits'] / max(1, x['days_available']), 2), axis = 1) 			# Feature engineering
 
-try:
-	
+try:	
 	f['Uteplats'] = df['Uteplats'] == 'Ja'
 
 except:
-
 	print('[INFO] The column Uteplats does not exist')
 
 
+print(df.info())
 print(df)
